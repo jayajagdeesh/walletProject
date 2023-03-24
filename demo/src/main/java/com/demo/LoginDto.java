aecef38be5fc261d8fd12a8ba959267e3c1dc065
Jayalakshmi <117960756+jayajagdeesh@users.noreply.github.com>
2023-03-24 21:28:10 +0530

    Added all files added manually

---
+++ demo/src/main/java/com/demo/LoginDto.java
@@ -0,0 +1,45 @@
+package com.walletone.demo;
+
+public class LoginDto {
+    String name;
+    String password;
+    String role;
+
+    public LoginDto() {
+    }
+
+    public LoginDto(String name, String password, String role) {
+        this.name = name;
+        this.password = password;
+        this.role = role;
+    }
+
+    public LoginDto(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
