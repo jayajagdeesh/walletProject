b18d1f0e0abc31344123c5da3c565480e68b7f54
Jayalakshmi <117960756+jayajagdeesh@users.noreply.github.com>
2023-03-24 21:36:22 +0530

    Delete CollectionEmployeeRepository.java

--- demo/src/main/java/com/demo/employee/CollectionEmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.demo.employee;
-
-public interface CollectionEmployeeRepository {
-    EmployeeDto addEmployee(EmployeeDto employee);
-    EmployeeDto getEmployee(Integer id);
-    EmployeeDto updateEmployee(EmployeeDto employeeDto);
-    EmployeeDto deleteEmployee(Integer employeeId);
-}
